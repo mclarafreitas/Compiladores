b5ea5a6e252ce7b2ec3c40a9195109cd95ea5ba4
Maria Clara Freitas <62397977+mclarafreitas@users.noreply.github.com>
2024-10-25 23:16:06 -0300

    Add files via upload

---
+++ Lab10/Ast/build/CMakeFiles/tradutor.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/maria/desktop/lab10/ast/ast.cpp" "CMakeFiles/tradutor.dir/ast.cpp.o" "gcc" "CMakeFiles/tradutor.dir/ast.cpp.o.d"
+  "/mnt/c/Users/maria/desktop/lab10/ast/checker.cpp" "CMakeFiles/tradutor.dir/checker.cpp.o" "gcc" "CMakeFiles/tradutor.dir/checker.cpp.o.d"
+  "/mnt/c/Users/maria/desktop/lab10/ast/error.cpp" "CMakeFiles/tradutor.dir/error.cpp.o" "gcc" "CMakeFiles/tradutor.dir/error.cpp.o.d"
+  "/mnt/c/Users/maria/desktop/lab10/ast/lexer.cpp" "CMakeFiles/tradutor.dir/lexer.cpp.o" "gcc" "CMakeFiles/tradutor.dir/lexer.cpp.o.d"
+  "/mnt/c/Users/maria/desktop/lab10/ast/parser.cpp" "CMakeFiles/tradutor.dir/parser.cpp.o" "gcc" "CMakeFiles/tradutor.dir/parser.cpp.o.d"
+  "/mnt/c/Users/maria/desktop/lab10/ast/symtable.cpp" "CMakeFiles/tradutor.dir/symtable.cpp.o" "gcc" "CMakeFiles/tradutor.dir/symtable.cpp.o.d"
+  "/mnt/c/Users/maria/desktop/lab10/ast/tradutor.cpp" "CMakeFiles/tradutor.dir/tradutor.cpp.o" "gcc" "CMakeFiles/tradutor.dir/tradutor.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
